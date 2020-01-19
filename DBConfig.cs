4807007c97106b4e08c4b20a348a50474e3955f7
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-01-19 20:57:43 +0200

    suka

---
+++ DBConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileChanger3
+{
+    class DBConfig
+    {
+        public static string Host = "82.146.37.127";
+        public static string User = "postgres";
+        public static string DBname = "postgres";
+        public static int Port = 5432;
+        public static string Password = "123456789";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FileChanger3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConfig dbconfig = new DBConfig();
+            DBWorker db = new DBWorker(DBConfig.Host, DBConfig.User, DBConfig.DBname, DBConfig.Port, DBConfig.Password);
+            db.connect();
+            Console.WriteLine("LLLLL");
+        }
+    }
+}
